ac66b16f9ee1a035aa454a2d70e91293a9680e50
Teacher <softmasterlab@hotmail.com>
2020-06-04 12:44:43 +0300

    Добавлены примеры на JavaScript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+function example1() {
+    alert('Привет, JavaScript');
+    let name = prompt('Как Вас зовут?');
+    alert('Будем знакомы, ' + name);
+    let choice = confirm('Хотите перейти на поисковик Bing?');
+    if (choice == true) {
+        window.location = 'https://www.bing.com';
+    } else {
+        alert('Ну не хотите и не надо!');
+    }
+}
+
+
 window.onload = function () {
     
-    alert('Привет, JavaScript');
+    let b1 = document.getElementById('b1');
+    b1.onclick = function () {
+        example1();
+    };
     
 };
+
